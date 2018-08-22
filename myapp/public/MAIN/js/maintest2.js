6b4b60de9e58e0c76d20e2919fd6480067a30348
tttttttttttzu <chelseatsa>
2018-08-22 16:36:22 +0800

    cart

--- myapp/public/MAIN/js/maintest2.js
+++ myapp/public/MAIN/js/maintest2.js
@@ -4,47 +4,48 @@ $(".toggle-box").click(function() {
   $(this).find(".toggle-text").toggle(300);
 });
 
+$(function(){
+   // 偵聽a click
+   $(".w3-col a").bind("click",function(){
+
+       var id=$(this).attr("data-id");
+
+       // 取得目標區塊的y座標
+       var target_top = $("#"+id).offset().top;
+
+     // 取得body物件 這種寫法在各瀏覽器上最保險
+       var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+
+
+       // 移動捲軸有動畫效果
+     		$body.animate({
+     			  scrollTop: target_top
+     		}, 500);
+
+   })
+})
+
+// 產品頁
+function openMenu(evt, menuName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+     tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-dark-grey";
+}
+document.getElementById("myLink").click();
+
 // on top按鈕
  $("#return-to-top").click(function(e){
       e.preventDefault();
       $('html, body').animate({scrollTop:0},800)
     })
-
-    $(function(){
-       // 偵聽a click
-       $(".w3-col a").bind("click",function(){
-
-           var id=$(this).attr("data-id");
-
-           // 取得目標區塊的y座標
-           var target_top = $("#"+id).offset().top;
-
-         // 取得body物件 這種寫法在各瀏覽器上最保險
-           var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
-
-
-           // 移動捲軸有動畫效果
-         		$body.animate({
-         			  scrollTop: target_top
-         		}, 500);
-
-       })
-    })
-    // 產品頁
-    function openMenu(evt, menuName) {
-      var i, x, tablinks;
-      x = document.getElementsByClassName("menu");
-      for (i = 0; i < x.length; i++) {
-         x[i].style.display = "none";
-      }
-      tablinks = document.getElementsByClassName("tablink");
-      for (i = 0; i < x.length; i++) {
-         tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
-      }
-      document.getElementById(menuName).style.display = "block";
-      evt.currentTarget.firstElementChild.className += " w3-dark-grey";
-    }
-    document.getElementById("myLink").click();
 
 
 
