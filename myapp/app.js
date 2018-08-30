51d02330562a3b9a93d7bafccca617e981085f66
kuomuchi <42135910+kuomuchi@users.noreply.github.com>
2018-08-30 11:10:20 +0800

    wahahahaha

--- myapp/app.js
+++ myapp/app.js
@@ -1,4 +1,5 @@
-
+const mongo = require("./mongodb.js")
+const ObjectId = require("mongodb").ObjectID
 
 var express = require('express');
 var app = express();
@@ -8,10 +9,20 @@ app.get('/', function (req, res) {
 });
 
 app.use(express.static('public/MAIN'));
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.post('/input', function (req, res) {
-  res.send('get request');
+  var date = new Date();
+	var monthDay = (date.getMonth() + 1) + "/" + date.getDate()
+	console.log({title: req.body.title, text: req.body.text, date: monthDay, day: date.getDay()})
+	mongo.insertMongo("form", {title: req.body.title, text: req.body.text, date: monthDay, day: date.getDay()})
+	res.redirect("/")
 });
+
+
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
